bb90115327cda978d3276aa31c1febb95f384d9a
xorbital <53773184+xorbital@users.noreply.github.com>
2022-06-13 12:48:04 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -5,18 +5,17 @@ APP_NAME = 'SZ2pdf'
 setup(
     name=APP_NAME,
     version='0.2.0',
-    py_modules=['SZ2pdf', 'conf_utils', 'output_utils'],
-    url='https://github.com/SiKreuz/SZ2pdf',
+    py_modules=['FR2pdf', 'conf_utils', 'output_utils'],
+    url='https://github.com/xorbital/FR2pdf',
     license='MIT',
-    author='Simon Kreuzer',
-    author_email='mail@monsi.org',
+    author='xorbital',
     description='This tool downloads the current newspaper as pdf from the "Sueddeutsche Zeitung" using your login '
                 'credentials.',
 
     python_requires='>=3.5',
     entry_points='''
     [console_scripts]
-    SZ2pdf=SZ2pdf:cli
+    FR2pdf=FR2pdf:cli
     ''',
     install_requires=[
         'mechanize~=0.4.5',
