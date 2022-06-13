f8940cae3bf787432d6ce105adeee9558f925a57
xorbital <53773184+xorbital@users.noreply.github.com>
2022-06-13 12:46:01 +0200

    Initial commit

--- SZ2pdf.py
+++ SZ2pdf.py
@@ -7,10 +7,16 @@ import mechanize
 import conf_utils
 from output_utils import e_print
 
+import sys, logging
+logger = logging.getLogger("mechanize")
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.setLevel(logging.DEBUG)
+
+
 # URLs
-LOGIN_URL = 'https://epaper.sueddeutsche.de/login'
-E_PAPER_URL = 'https://epaper.sueddeutsche.de/'
-DOWNLOAD_URL_PREFIX = 'https://epaper.sueddeutsche.de/download/'
+LOGIN_URL = 'http://epaper.fr.de/anmelden'
+E_PAPER_URL = 'https://epaper.fr.de/'
+DOWNLOAD_URL_PREFIX = 'https://epaper.fr.de/download/issue/'
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -27,13 +33,15 @@ def cli(edition, username, password, download_dir):
         exit(1)
 
     br = mechanize.Browser()
+    br.set_handle_robots(False)
+    # robots.txt prevents mechanize from working otherwise
 
     # login
     print('Logging in...')
     br.open(LOGIN_URL)
     br.select_form(nr=0)
-    br.form['login'] = username
-    br.form['password'] = password
+    br.form['email'] = username
+    br.form['passwort'] = password
     br.submit()
 
     # login validation
@@ -42,7 +50,12 @@ def cli(edition, username, password, download_dir):
         exit(1)
     else:
         print('Login successful.')
-
+    # FR decided that 3 logins is enough for each user.
+    if 'Sie haben sich zu oft angemeldet.' in br.response().read().decode('utf-8'):
+        print('Maximum number of logins exceeded. Destroying old sessions.')
+        br.open("https://meinabo.fr.de/abo/benutzer/alteSitzungBeenden")
+    else:
+        print('Maximum number of logins not exceeded.')    
     # open selected edition
     try:
         br.open(E_PAPER_URL + edition)
@@ -57,7 +70,7 @@ def cli(edition, username, password, download_dir):
     # download pdf
     print('Downloading "' + edition + '" of the current newspaper from ' + download_url)
     file_path_regex = datetime.now().strftime(
-        click.format_filename(download_dir) + '/%Y_%m_%d_SZ-' + edition + '.pdf')
+        click.format_filename(download_dir) + '/%Y_%m_%d_FR-' + edition + '.pdf')
     file_path = br.retrieve(download_url, file_path_regex)[0]
 
     br.close()
